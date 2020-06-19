4359a868c0b1a3835e1b19414b122e3c486b4fac
bobrock8 <aleksandarilic990@gmail.com>
2020-06-19 18:21:11 +0200

    number input initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
+import NumberInput from './components-library/basic-components/NumberInput';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumberInput class="acaclass" name="acaname" placeholder="acaplace" min="10" max="20"/>
     </div>
   );
 }
---
+++ src/components-library/basic-components/NumberInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NumberInput(props) {
+    return (
+            <input 
+                type="number"
+                name={props.name} 
+                className={props.class}
+                placeholder={props.placeholder}
+                min={props.min}
+                max={props.max}
+            />
+         )
+    ;
+}
+
+export default NumberInput;
