ba8891d7868943b583515cefbced1b40d92d0db5
Goran Spasojevic <goky91@gmail.com>
2020-07-11 14:26:46 +0200

    Added basic checkbox input component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import NumberInput from './components-library/basic-components/NumberInput';
+import CheckboxInput from './components-library/basic-components/CheckboxInput';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NumberInput class="acaclass" name="acaname" placeholder="acaplace" min="10" max="20"/>
+        <h2>Number Input</h2>
+        <NumberInput class="acaclass" name="acaname" placeholder="acaplace" min="10" max="20"/>
+        <hr />
+        <h2>Checkbox Input</h2>
+        <CheckboxInput class="goran-class" name="goran-name" />
     </div>
   );
 }
