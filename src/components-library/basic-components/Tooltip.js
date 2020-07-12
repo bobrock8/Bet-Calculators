35d2a435098064f009b43d8889d167357a301c7d
Goran Spasojevic <goky91@gmail.com>
2020-07-12 10:24:38 +0200

    Fixed tooltip custom class logic.

--- src/components-library/basic-components/Tooltip.js
+++ src/components-library/basic-components/Tooltip.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 function Tooltip ( props ) {
+    let customClass = props.customClass ? props.customClass : "";
+
     return (
-            <span className={"tooltip " + props.customClass} >
+            <span className={"tooltip " + customClass} >
                 {props.children}
                 <span className="tooltip-text" >{props.tooltipText}</span>
             </span>
